cddc6278298c99704e74a6856b68f2804eba8e83
joshahuynh <joshahuynh@gmail.com>
2022-04-11 18:20:42 -0500

    intern and manager tests

---
+++ __tests__/intern.test.js
@@ -0,0 +1,22 @@
+const Intern = require("../lib/intern");
+
+test('create intern object', ()=>{
+    const intern = new Intern('josh', 12345, 'emailtest@gmail.com', 'UT')
+    
+    expect(intern.name).toEqual(expect.any(String))
+    expect(intern.id).toEqual(expect.any(Number))
+    expect(intern.email).toEqual(expect.any(String))
+    expect(intern.school).toEqual(expect.any(String))
+});
+
+test('check for getRole()', ()=>{
+    const intern = new Intern('josh', 12345, 'emailtest@gmail.com', 'UT')
+
+    expect(intern.getRole()).toBe('Intern');
+});
+
+test('check for getSchool()', ()=>{
+    const intern = new Intern('josh', 12345, 'emailtest@gmail.com', 'UT')
+
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
---
+++ __tests__/manager.test.js
@@ -0,0 +1,22 @@
+const Manager = require("../lib/manager");
+
+test('create manager object', ()=>{
+    const manager = new Manager('josh', 12345, 'emailtest@gmail.com', 5129543103)
+    
+    expect(manager.name).toEqual(expect.any(String))
+    expect(manager.id).toEqual(expect.any(Number))
+    expect(manager.email).toEqual(expect.any(String))
+    expect(manager.officeNumber).toEqual(expect.any(Number))
+});
+
+test('check for getRole()', ()=>{
+    const manager = new Manager('josh', 12345, 'emailtest@gmail.com', 5129543103)
+
+    expect(manager.getRole()).toBe('Manager');
+});
+
+test('check for getOfficeNumber()', ()=>{
+    const manager = new Manager('josh', 12345, 'emailtest@gmail.com', 5129543103)
+
+    expect(manager.getOfficeNumber()).toEqual(expect.any(Number));
+});
