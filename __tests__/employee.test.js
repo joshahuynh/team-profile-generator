39260ce16ebae3e405074d6e19d084d7567b478a
joshahuynh <joshahuynh@gmail.com>
2022-04-11 18:19:04 -0500

    employee and engineer tests

---
+++ __tests__/employee.test.js
@@ -0,0 +1,33 @@
+const Employee = require("../lib/employee");
+
+test('create employee object', ()=>{
+    const employee = new Employee('josh', 12345, 'emailtest@gmail.com')
+    
+    expect(employee.name).toEqual(expect.any(String))
+    expect(employee.id).toEqual(expect.any(Number))
+    expect(employee.email).toEqual(expect.any(String))
+});
+
+test('check for getName()', ()=>{
+    const employee = new Employee('josh', '12345', 'emailtest@gmail.com')
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
+});
+
+test('check for getId()', ()=>{
+    const employee = new Employee('josh', '12345', 'emailtest@gmail.com')
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+});
+
+test('check for getEmail()', ()=>{
+    const employee = new Employee('josh', '12345', 'emailtest@gmail.com')
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+test('check for getRole()', ()=>{
+    const employee = new Employee('josh', '12345', 'emailtest@gmail.com')
+
+    expect(employee.getRole()).toBe('Employee');
+});
---
+++ __tests__/engineer.test.js
@@ -0,0 +1,22 @@
+const Engineer = require("../lib/engineer");
+
+test('create engineer object', ()=>{
+    const engineer = new Engineer('josh', 12345, 'emailtest@gmail.com', 'joshahuynh')
+    
+    expect(engineer.name).toEqual(expect.any(String))
+    expect(engineer.id).toEqual(expect.any(Number))
+    expect(engineer.email).toEqual(expect.any(String))
+    expect(engineer.github).toEqual(expect.any(String))
+});
+
+test('check for getRole()', ()=>{
+    const engineer = new Engineer('josh', 12345, 'emailtest@gmail.com', 'joshahuynh')
+
+    expect(engineer.getRole()).toBe('Engineer');
+});
+
+test('check for getGithub()', ()=>{
+    const engineer = new Engineer('josh', 12345, 'emailtest@gmail.com', 'joshahuynh')
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
